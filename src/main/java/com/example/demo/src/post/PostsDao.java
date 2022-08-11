e9be9f0aaef113e4d11751d05f5474e21ec8ae26
lana <artist158@naver.com>
2022-08-11 22:55:53 +0900

    2022.08.11 jwt token ver.1

---
+++ src/main/java/com/example/demo/src/post/PostsDao.java
@@ -0,0 +1,20 @@
+package com.example.demo.src.post;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.stereotype.Repository;
+
+import javax.sql.DataSource;
+
+@Repository
+public class PostsDao {
+
+    private JdbcTemplate jdbcTemplate;
+
+    @Autowired
+    public void setDataSource(DataSource dataSource){
+        this.jdbcTemplate = new JdbcTemplate(dataSource);
+    }
+
+
+}
